630e48aa9dd80904939b6d7cfdda8893095cd1bd
heyitscarl <carl.nw.neben@gmail.com>
2025-08-15 00:39:28 +0200

    feat: add basic game loop with input handling and quitting

--- crates/term-invaders/src/main.rs
+++ crates/term-invaders/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+use crossterm::event::KeyCode;
+use term_invaders_engine::input::{input_system, Inputs};
+
+fn main() -> Result<()> {
+    let mut stdout = term_invaders_engine::render::prepare()?;
+    
+    // Enter Loop 
+
+    loop {
+        // Gather Input
+        let inputs: Inputs = input_system()?;
+
+        // Apply Input
+        if inputs.contains(&KeyCode::Char('q')) {
+            break;
+        }
+    }
+    
+    // Cleanup
+    
+    term_invaders_engine::render::cleanup(&mut stdout)?;
+    Ok(())
 }
